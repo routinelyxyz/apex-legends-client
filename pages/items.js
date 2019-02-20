1c1134d26f1535b10b83e81fd3c5ab0352b91705
alk831 <adam54111@gmail.com>
2019-02-20 17:38:23 +0100

    update style of submenu

--- pages/items.js
+++ pages/items.js
@@ -128,18 +128,36 @@ const SubMenu = styled.nav`
 `
 const NavLink = styled.a`
   margin-right: 35px;
-  font-size: 20px;
+  font-size: 18px;
   color: ${props => props.active ? '#fff' : 'rgb(99, 99, 134)'};
   position: relative;
+  padding: 20px;
+  &:hover {
+    color: #fff;
+  }
+  &:before {
+    content: ${props => props.active ? "''" : ''};
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    background: radial-gradient(
+      #8289ff 0, transparent 80%
+    ) no-repeat;
+    background-position-y: 20px;
+    left: 0;
+    bottom: -10px;
+    opacity: .15;
+  }
   &:after {
     content: ${props => props.active ? "''" : ''};
     height: 3px;
     width: 100%;
-    border-radius: 6px;
+    border-radius: 8px;
     background: #6770FA;
     position: absolute;
     left: 0;
-    bottom: -25px;
+    bottom: -10px;
+    /* box-shadow: 0 0 10px 5px #6770FA; */
   }
 `
 const ItemsContainer = styled.article`
