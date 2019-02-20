4c8836d6560b9335482a9406c27885e89c7bee72
alk831 <adam54111@gmail.com>
2019-02-20 21:54:56 +0100

    update header component

--- components/Header/index.js
+++ components/Header/index.js
@@ -2,24 +2,34 @@ import css from './style.scss';
 import Link from 'next/link';
 import styled from 'styled-components';
 
+import RouteLink from '../RouteLink';
+import Input from '../../reusable/Input';
+
 const Header = () => (
-  <header className={css.header__container}>
+  <header className={css.container}>
+    <div className={css.logo}>
+      Apex-Legends.win
+    </div>
     <nav className={css.header__links}>
-      <Link href="/">
-        <StyledLink>Home</StyledLink>
+      <Link href="/" passHref>
+        <RouteLink>Home</RouteLink>
       </Link>
-      <Link href="/items">
-        <StyledLink>Items</StyledLink>
+      <Link href="/items" passHref>
+        <RouteLink>Items</RouteLink>
       </Link>
     </nav>
+    <div className={css.searcher}>
+      <Input
+        placeholder="Player nickname..."
+      />
+    </div>
   </header>
 );
 
-const StyledLink = styled.a`
-  font-size: 16px;
-  display: block;
-  cursor: pointer;
-  font-weight: 400;
+const Container = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `
 
 export default Header;
