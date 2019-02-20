cc62a7649af6940e7c97d3683ca616428989cb06
alk831 <adam54111@gmail.com>
2019-02-20 22:21:09 +0100

    add searcher component and update header comp

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,31 +1,46 @@
 import css from './style.scss';
 import Link from 'next/link';
 import styled from 'styled-components';
+import { useState } from 'react';
 
 import RouteLink from '../RouteLink';
 import Input from '../../reusable/Input';
+import { Searcher } from '../../reusable/Searcher';
 
-const Header = () => (
-  <header className={css.container}>
-    <div className={css.logo}>
-      Apex-Legends.win
-    </div>
-    <nav className={css.header__links}>
-      <Link href="/" passHref>
-        <RouteLink>Home</RouteLink>
-      </Link>
-      <Link href="/items" passHref>
-        <RouteLink>Items</RouteLink>
-      </Link>
-    </nav>
-    <div className={css.searcher}>
-      <Input
-        placeholder="Player nickname..."
-      />
-    </div>
-  </header>
-);
+const Header = () => {
+  const [phrase, setPhrase] = useState('');
+  const results = [
+    { name: 'Ninja' },
+    { name: 'Shroud' },
+    { name: 'Lirik' }
+  ]
 
+  return (
+    <header className={css.container}>
+      <div className={css.logo}>
+        Apex-Legends.win
+      </div>
+      <nav className={css.header__links}>
+        <Link href="/" passHref>
+          <RouteLink>Home</RouteLink>
+        </Link>
+        <Link href="/items" passHref>
+          <RouteLink>Items</RouteLink>
+        </Link>
+      </nav>
+      <div className={css.searcher}>
+        <Searcher
+          placeholder="Player nickname..."
+          value={phrase}
+          onChange={e => setPhrase(
+            e.target.value
+          )}
+          results={results}
+        />
+      </div>
+    </header>
+  );
+}
 const Container = styled.header`
   display: flex;
   align-items: center;
