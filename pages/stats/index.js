fd9369f862ebde8e82d32e7604721618d15da6f9
alk831 <adam54111@gmail.com>
2019-03-03 22:24:06 +0100

    fetch stats on countdown, render counter time

--- pages/stats/index.js
+++ pages/stats/index.js
@@ -13,17 +13,21 @@ import { ProgressRing } from '../../components/ProgressRing';
 import { HorizontalNav } from '../../reusable/HorizontalNav';
 import { LegendStats } from '../../components/LegendStats';
 
-const getStats = async () => {
-  return { stats: {} }
+const getStats = async ({ platform, name, id = '' }) => {
+  const url = getUrl(`/stats/${platform}/${encodeURI(name)}?id=${id}`);
+  const res = await fetch(url);
+  const stats = await res.json();
+  return { stats: stats.stats };
 }
 
 const initialTs = getTs();
-const countdown = 300;
+const countdown = 179;
 
 const StatsPage = ({ name, url, ...props }) => {
   const [stats, setStats] = useState(() => props.stats.stats);
   const [now, setNow] = useState(() => initialTs);
   const [to, setTo] = useState(() => initialTs + countdown);
+  const [isUpdating, setUpdating] = useState(false);
   const counter = to - now;
 
   useEffect(() => {
@@ -35,13 +39,27 @@ const StatsPage = ({ name, url, ...props }) => {
 
   useEffect(() => {
     if (counter < 0) {
-      getStats()
+      const { platform, name, id } = stats;
+
+      if (isUpdating) return;
+      setUpdating(true);
+
+      getStats(stats)
       .then(({ stats }) => {
+        setStats(stats);
+        setUpdating(false);
         setTo(getTs() + countdown);
       })
       .catch(console.log)
     }
   }, [counter]);
+
+  const updateIn = () => {
+    const seconds = counter % 60;
+    const minutes = Math.floor(counter / 60);
+    if (minutes <= 0 && seconds <= 0) return 'Just now';
+    return (minutes && `${minutes} min. ` || '') + `${seconds} sec.`;
+  }
 
   const lvlProps = useSpring({
     from: { lvl: 0 },
@@ -66,7 +84,7 @@ const StatsPage = ({ name, url, ...props }) => {
         </div>
         <div>
           <h1 className={css.name}>
-            {name} {counter}
+            {name}
           </h1>
           <p className={css.lvl_container}>
             <span className={css.lvl_title}>
@@ -78,6 +96,12 @@ const StatsPage = ({ name, url, ...props }) => {
               {lvlProps.lvl.interpolate(v => v.toFixed())}
             </animated.span>
           </p>
+        </div>
+        <div className={css.update_container}>
+          <p className={css.update_title}>
+            Update in:
+          </p>
+          {updateIn()}
         </div>
       </div>
       <HorizontalNav>
