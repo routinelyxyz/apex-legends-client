09edca9e711f0aa7fde988d0bea807b24d6051a0
alk831 <adam54111@gmail.com>
2019-03-06 16:14:44 +0100

    upgrade next config with workbox

--- next.config.js
+++ next.config.js
@@ -1,16 +1,22 @@
 const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
 const withSass = require('@zeit/next-sass');
+const withWorkbox = require('next-workbox');
+const compose = require('next-compose');
 
-module.exports = withSass({
-  cssModules: true,
-  webpack: (config) => {
-    const newConfig = { ...config };
-    newConfig.plugins = [
-      ...config.plugins,
-      new FilterWarningsPlugin({
-        exclude: /mini-css-extract-plugin[^]*Conflicting order between:/,
-      }),
-    ];
-    return newConfig;
-  },
-});
+module.exports = compose([
+  [withSass, { cssModules: true }],
+  [withWorkbox, {
+    workbox: { registerSW: true }
+  }],
+  {
+    webpack: config => ({
+      ...config,
+      plugins: [
+        ...config.plugins,
+        new FilterWarningsPlugin({
+          exclude: /mini-css-extract-plugin[^]*Conflicting order between:/,
+        })
+      ]
+    })
+  }
+]);
