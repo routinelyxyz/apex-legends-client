c4946f5d2a71ef2dca959cd6d13f7fa94f6556ea
alk831 <adam54111@gmail.com>
2019-03-07 16:32:53 +0100

    update rwd of legends page

--- components/Legend/index.js
+++ components/Legend/index.js
@@ -1,18 +1,24 @@
 import css from './style.scss';
 import { HOST_URL, STATIC } from '../../helpers';
+import Link from 'next/link';
 
-const Legend = ({ legend }) => {
+export const Legend = ({ legend }) => {
   return (
-    <li className={css.container}>
-      <img
-        src={STATIC + legend.img}
-        className={css.img}
-      />
-      <p className={css.name}>
-        {legend.name}
-      </p>
+    <li>
+      <Link
+        href={`/legends/legend?slug=${legend.slug}`}
+        as={`/legends/${legend.slug}`}
+      > 
+        <a className={css.container}>
+          <img
+            src={STATIC + legend.img}
+            className={css.img}
+          />
+          <p className={css.name}>
+            {legend.name}
+          </p>
+        </a>
+      </Link>
     </li>
   )
 }
-
-export default Legend;
--- pages/legends/index.js
+++ pages/legends/index.js
@@ -1,12 +1,11 @@
 import css from './style.scss';
 import 'isomorphic-unfetch';
 import { HOST_URL, STATIC } from '../../helpers';
-import Link from 'next/link';
 import Head from 'next/head';
 
-import Legend from '../../components/Legend';
+import { Legend } from '../../components/Legend';
 
-const Legends = ({ legends }) => {
+const LegendsPage = ({ legends }) => {
   return (
     <article>
       <Head>
@@ -16,28 +15,16 @@ const Legends = ({ legends }) => {
         Legends
       </h1>
       <ul className={css.legends}>
-        {legends.map(legend => (
-          <Link
-            key={legend.id}
-            href={`/legends/legend?slug=${legend.slug}`}
-            as={`/legends/${legend.slug}`}
-          > 
-            <a>
-              <Legend
-                legend={legend}
-              />
-            </a>
-          </Link>
-        ))}
+        {legends.map(legend => <Legend legend={legend}/>)}
       </ul>
     </article>
   )
 }
 
-Legends.getInitialProps = async () => {
+LegendsPage.getInitialProps = async () => {
   const data = await fetch(HOST_URL + '/legends');
   const legends = await data.json();
   return { legends };
 }
 
-export default Legends;
+export default LegendsPage;
