01bdc0ee85c59e60c87345a8f903bd186f93e86b
alk831 <adam54111@gmail.com>
2019-03-21 20:27:02 +0100

    update useDevice hook and modal provider scroll

--- helpers/context.js
+++ helpers/context.js
@@ -1,18 +1,22 @@
 import React, { useState, useMemo, useEffect } from 'react';
+import { useDevice } from '../hooks';
 
 export const ModalContext = React.createContext();
 
 export const ModalProvider = ({ children }) => {
   const [opened, setOpened] = useState(false);
+  const { isDesktop } = useDevice();
 
   const value = useMemo(() => ({
     opened,
-    scroll: !opened,
-    setOpened: (status = !opened) => setOpened(status)
-  }), [opened]);
+    scroll: !opened || !isDesktop,
+    setOpened: (status = !opened, options = {}) => {
+      setOpened(status);
+    }
+  }), [opened, isDesktop]);
 
   useEffect(() => {
-    document.body.classList.toggle('hidden_scroll', opened);
+    document.body.classList.toggle('hidden_scroll', value.scroll);
   }, [opened]);
 
   return (
