9d65ff6116a717d69666bb4cf83dd0d6c8ddb535
alk831 <adam54111@gmail.com>
2019-03-22 19:31:21 +0100

    scaffold match history group by day

--- components/StatsHistory/index.js
+++ components/StatsHistory/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import axios from 'axios';
 import dayjs from 'dayjs';
 import css from './style.scss';
@@ -14,17 +14,45 @@ export const StatsHistory = ({ player }) => {
 
   useEffect(() => {
     fetchHistory(player)
-      .then(history => setHistory(history))
+      .then(history => {
+        const withDay = history.map(match => ({
+          ...match,
+          day: dayjs(match.date).format('YYYY-MM-DD')
+        }))
+        setHistory(withDay);
+      })
       .catch(console.error)
   }, []);
+
+  const groupedByDay = useMemo(() => history
+    .reduce((grouped, match, index, original) => {
+      const lastGroupIndex = grouped.length - 1;
+      const { day } = match;
+
+      if (index === 0 || match.day !== original[index - 1].day) {
+        grouped.push({ day, matches: [match] });
+      } else {
+        grouped[lastGroupIndex].matches.push(match);
+      }
+
+      return grouped;
+    }, [])
+  , [history]);
 
   return (
     <div className={css.container}>
       <ul>
-        {history.map(record => (
-          <li key={record.id}>
-            {record.kills}
-            <p>{dayjs(record.date).fromNow()}</p>
+        {groupedByDay.map(dailyHistory => (
+          <li key={dailyHistory.day}>
+            <h4>{dailyHistory.day}</h4>
+            <ul>
+            {dailyHistory.matches.map(match => (
+              <li key={match.id}>
+                <h3>{match.legend.name}</h3>
+                <p>{dayjs(match.date).fromNow()}</p>
+              </li>
+            ))}
+            </ul>
           </li>
         ))}
       </ul>
