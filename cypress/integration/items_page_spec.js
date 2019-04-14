f34e934120b3c4862997c49c21e3ca8ccbd1c5fa
alk831 <adam54111@gmail.com>
2019-04-14 22:54:34 +0200

    test: refactor items page spec

--- cypress/integration/items_page_spec.js
+++ cypress/integration/items_page_spec.js
@@ -15,7 +15,7 @@ describe('Items page', function() {
     cy.visit('/items');
   });
 
-  it('', function() {
+  it('filtering items work properly', function() {
     cy.clock();
     cy.get('article input[type=text]').type('mozambique');
     cy.get(grid `div p`).contains('Mozambique Shotgun');
@@ -27,10 +27,22 @@ describe('Items page', function() {
     cy.get(grid `div p`).contains('Prowler Burst PDW');
 
     cy.get('article input[type=text]').clear().type('ma');
-    cy.get(grid `div`).its('length').should('be.lte', 4);
+    cy.get(grid `> div`).its('length').should('be.lte', 4);
     cy.get(category `label`).eq(4).click();
-    cy.tick(250);
-    cy.get(grid `div`).its('length').should('eq', 1);
+    cy.get(grid `> div`).its('length').should('eq', 1);
+
+    cy.get(category `label`).eq(3).click();
+    cy.get(grid `> div`).its('length').should('be.gte', 1);
+
+    cy.get(`h3:contains('Direction') ~ label`).click();
+    cy.get(grid `> div:first-child p`).contains('Wingman');
+
+    cy.get(`h3:contains('Direction') ~ label`).click();
+    cy.get(grid `> div:last-child p`).contains('Wingman');
+
+    cy.get(ammo `label span:contains('Energy')`).eq(0).click();
+    cy.get(ammo `label span:contains('Heavy')`).eq(0).click();
+    cy.get(grid `> div:first-child p`).contains('Wingman');
   });
 
 });
