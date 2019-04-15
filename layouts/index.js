9719afdce0168b09b751e644412e00624eefd8b9
alk831 <adam54111@gmail.com>
2019-04-15 16:02:49 +0200

    cleanup layout comp

--- layouts/index.js
+++ layouts/index.js
@@ -1,25 +1,11 @@
 import css from '../assets/css/index.scss';
-import dynamic from 'next/dynamic';
 import React from 'react';
-import { useTransition, useSpring, animated } from 'react-spring';
 
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
-
-// const ItemsLayout = dynamic(() => import('./items'));
 import ItemsLayout from './items';
 
 const MainLayout = ({ children, route }) => {
-
-  const pageTransitions = useTransition(route, r => r, {
-    // from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
-    // enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
-    // leave: { opacity: 0, transform: 'translate3d(-50%,0,0)', position: 'absolute' },
-    from: { opacity: 0 },
-    enter: { opacity: 1 },
-    leave: { opacity: 0 }
-  });
-
   return (
     <div className={css.app_container}>
       <Header route={route}/>
@@ -34,30 +20,7 @@ const MainLayout = ({ children, route }) => {
       </main>
       {/* <Footer/> */}
     </div>
-  )
-
-  return (
-    <div className={css.app_container}>
-      <Header/>
-      {pageTransitions.map(({ item, key, props }) => {
-        return item === route && (
-          <animated.main
-            className={css.app_content}
-            style={props}
-            key={key}
-          >
-            {/* {route.includes('/items')
-              ? <ItemsLayout
-                  children={children}
-                  route={route}
-                />
-              : children
-            } */}
-          </animated.main>
-        )
-      })}
-    </div>
-  )
+  );
 };
 
 export default MainLayout;
