b68c8e262c8dd6791fdf0cd2b668c5f61b05b2df
alk831 <adam54111@gmail.com>
2019-04-17 15:04:02 +0200

    scaffold jest test setup

---
+++ __tests__/phrase_selector.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { create } from "react-test-renderer";
+import { PhraseSelector } from '../reusable/PhraseSelector';
+
+test('it matches the snapshot', () => {
+  const component = create(
+    <PhraseSelector
+      value="Rockalone"
+      phrase="alone"
+    />
+  );
+  expect(component.toJSON()).toMatchSnapshot();
+    
+  component.update(
+    <PhraseSelector
+      value="veryLongPhraseWith Some Text"
+      phrase="longPhr"
+    />
+  );
+  expect(component.toJSON()).toMatchSnapshot();
+});
