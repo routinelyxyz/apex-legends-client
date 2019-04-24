1631cd19e0f8e5f31e5119f1d33c177b2c995576
alk831 <adam54111@gmail.com>
2019-04-24 11:53:56 +0200

    dont display legend stats if each value is null

--- components/LegendStats/index.js
+++ components/LegendStats/index.js
@@ -4,6 +4,12 @@ import { getStatic, statsProps } from '../../helpers';
 import { LegendStatsValue } from '../LegendStatsValue';
 
 export const LegendStats = ({ stats }) => {
+  const availaibleProps = statsProps.legend.filter(prop => stats[prop].value != null);
+  
+  if (!availaibleProps.length) {
+    return null;
+  }
+
   return (
     <div className={`box ${css.container}`}>
       <div className={css.legend_container}>
@@ -16,7 +22,7 @@ export const LegendStats = ({ stats }) => {
         />
       </div>
       <ul className={css.stats_list}>
-        {statsProps.legend.map(prop => (
+        {availaibleProps.map(prop => (
           stats[prop].value != null && (
             <LegendStatsValue
               key={prop}
