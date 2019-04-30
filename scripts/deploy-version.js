26849e01121d78abfe1c7d49f39bae702dc5a270
alk831 <adam54111@gmail.com>
2019-04-30 12:49:00 +0200

    test script on ci

--- scripts/deploy-version.js
+++ scripts/deploy-version.js
@@ -3,7 +3,8 @@ const host = 'apex-legends.win';
 
 (() => {
   const { CI_COMMIT_REF_SLUG: version, NOW_TOKEN } = process.env;
-  
+  console.log(process.env);
+
   if (version != null) {
     const normalizedVersion = version.substr(1, version.length).replace(/./g, '-');
     const url = `staging-${normalizedVersion}.${host}`;
