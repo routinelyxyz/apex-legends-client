63ae67ad9109fa1d8eb436a0bddc2a25617cf239
alk831 <adam54111@gmail.com>
2019-04-30 12:42:28 +0200

    create version deploy script

---
+++ scripts/deploy-version.js
@@ -0,0 +1,13 @@
+const shell = require('shelljs');
+const host = 'apex-legends.win';
+
+(() => {
+  const { CI_COMMIT_REF_SLUG: version, NOW_TOKEN } = process.env;
+  
+  if (version != null) {
+    const normalizedVersion = version.substr(1, version.length).replace(/./g, '-');
+    const url = `staging-${normalizedVersion}.${host}`;
+    shell.echo(`Creating alias of ${url} for staging.${host}`);
+    shell.exec(`npx now alias staging.apex-legends.win ${url} --token=${NOW_TOKEN}`);
+  }
+})();
