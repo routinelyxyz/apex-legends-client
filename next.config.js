851ab3ca241ef61af21fc858f842a40fd984f3a7
alk831 <adam54111@gmail.com>
2019-06-05 21:06:24 +0200

    cleanup next config [skip ci]

--- next.config.js
+++ next.config.js
@@ -1,17 +1,9 @@
 const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
 const withSass = require('@zeit/next-sass');
-const withWorkbox = require('next-workbox');
-const compose = require('next-compose');
 const withPlugins = require('next-compose-plugins');
-const nextOffline = require('next-offline') 
-// const withOffline = require('next-offline');
-const NextWorkboxWebpackPlugin = require('next-workbox-webpack-plugin');
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-const withManifest = require('next-manifest');
+const nextOffline = require('next-offline');
 const { resolve } = require('path');
-const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
 const isProduction = process.env.NODE_ENV === 'production';
-
 
 const CDN_URL = 'https://api.apex-legends.win';
 
@@ -48,15 +40,6 @@ const nextConfig = {
       }
     ]
   },
-}
-
-const workbox = {
-  runtimeCaching: [
-    {
-      urlPattern: 'http://localhost:4000/stats/pc/rockalone',
-      handler: 'cacheFirst'
-    }
-  ]
 }
 
 const manifest = {
@@ -106,19 +89,13 @@ module.exports = withPlugins(
   {
     target: isProduction ? 'serverless' : 'server',
     webpack(config, { isServer, dev, buildId, config: { distDir }}) {
-      const plugins = [...config.plugins];
-
-      if (false && !isServer && !dev) {
-        config.plugins.push(
-          new NextWorkboxWebpackPlugin({ distDir, buildId, ...workbox })
-        );
-      }
-
-      plugins.push(
+      
+      const plugins = [
+        ...config.plugins,
         new FilterWarningsPlugin({
           exclude: /mini-css-extract-plugin[^]*Conflicting order between:/,
         })
-      );
+      ];
 
       return {
         ...config,
