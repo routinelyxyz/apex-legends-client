d6d69cb6cb0e79ad4057325388af45536214e0d7
alk831 <adam54111@gmail.com>
2019-06-05 10:56:53 +0200

    refactor utils

--- util/fetchify.js
+++ util/fetchify.js
@@ -6,8 +6,5 @@ export const fetchify = {
   get(url, query = {}) {
     const href = HOST_URL + url + qs.stringify(query, true);
     return fetch(href);
-  },
-  async post(url) {
-
   }
 }
--- util/index.js
+++ util/index.js
@@ -1,3 +1,4 @@
+export { applyCss as applyCss } from '../helpers';
 
 export const debounce = (delay = 250, timeout) => fn => {
   clearTimeout(timeout);
@@ -7,12 +8,15 @@ export const debounce = (delay = 250, timeout) => fn => {
 
 export const round = (val, scale = 10) => Math.round(val * scale) / scale;
 
-export const parsePercent = val => 
-  val < 0
-    ? 0
-    : val > 100
-      ? 100
-      : val;
+export const parsePercent = (number) => {
+  if (number < 0) {
+    return 0;
+  }
+  if (number > 100) {
+    return 100;
+  }
+  return number;
+}
 
 export const useDebounce = (delay = 250, timeout) => [
   fn => {
@@ -23,11 +27,6 @@ export const useDebounce = (delay = 250, timeout) => [
 ];
 
 export const getTs = () => Math.floor(Date.now() / 1000); 
-
-export { applyCss as applyCss } from '../helpers';
-
-export const useDispatch = dispatch => (type, payload, meta) => 
-  dispatch({ type, payload, meta });
 
 export const filterUnique = (value, index, self) => self.indexOf(value) === index;
 
