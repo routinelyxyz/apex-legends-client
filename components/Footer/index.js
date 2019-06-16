6be5cfa67f3dbd3260fa008890895de6a8b78651
alk831 <adam54111@gmail.com>
2019-06-16 21:45:11 +0200

    remove old js components [skip ci]

--- components/Footer/index.js
+++
@@ -1,9 +0,0 @@
-import css from './style.scss';
-
-const year = new Date().getFullYear();
-
-export const Footer = () => (
-  <footer className={css.container}>
-    {year} Apex-Legends.win. Apex Legends is a registered trademark of EA.
-  </footer>
-);
