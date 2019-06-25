e550754b5df8b3baed18683037d9cc0798e561c7
alk831 <adam54111@gmail.com>
2019-06-25 19:39:25 +0200

    move reducer to separate module

--- hooks/useFetch/index.ts
+++ hooks/useFetch/index.ts
@@ -1,51 +1,5 @@
 import { useReducer, useEffect, DependencyList, Reducer } from "react";
-
-interface FetchState<T> {
-  isFetching: boolean
-  isError: boolean
-  response: Response | null
-  data: T | null
-  controller: AbortController | null
-}
-
-const initialState = {
-  isFetching: true,
-  isError: false,
-  response: null,
-  data: null,
-  controller: null
-}
-
-function fetchReducer<T>(
-  state: FetchState<T>,
-  action: FetchAction<T>
-): FetchState<T> {
-  switch(action.type) {
-    case 'FETCH_REQUESTED': return {
-      ...state,
-      isFetching: true,
-      isError: false,
-      controller: action.payload
-    }
-    case 'FETCH_SUCCEEDED': return {
-      ...state,
-      response: action.payload.response,
-      data: action.payload.data,
-      isFetching: false,
-      isError: false,
-      controller: null
-    }
-    case 'FETCH_FAILED': return {
-      ...state,
-      response: action.payload.response,
-      isFetching: false,
-      isError: true,
-      data: null,
-      controller: null
-    }
-    default: throw Error;
-  }
-}
+import { fetchReducer, initialState, FetchAction, FetchState } from "./reducer";
 
 export function useFetch<T>(
   url: string,
@@ -106,28 +60,3 @@ interface UseFetchResult<T> {
   data: T | null
   cancel: AbortController['abort'] | null
 }
-
-interface FetchRequested {
-  type: 'FETCH_REQUESTED'
-  payload: AbortController
-}
-
-interface FetchSucceeded<T> {
-  type: 'FETCH_SUCCEEDED'
-  payload: {
-    data: T
-    response: Response
-  }
-}
-
-interface FetchFailed {
-  type: 'FETCH_FAILED'
-  payload: {
-    response: Response
-  }
-}
-
-type FetchAction<T> =
-  | FetchRequested
-  | FetchSucceeded<T>
-  | FetchFailed;
---
+++ hooks/useFetch/reducer.ts
@@ -0,0 +1,72 @@
+
+export interface FetchState<T> {
+  isFetching: boolean
+  isError: boolean
+  response: Response | null
+  data: T | null
+  controller: AbortController | null
+}
+
+export const initialState = {
+  isFetching: true,
+  isError: false,
+  response: null,
+  data: null,
+  controller: null
+}
+
+export function fetchReducer<T>(
+  state: FetchState<T>,
+  action: FetchAction<T>
+): FetchState<T> {
+  switch(action.type) {
+    case 'FETCH_REQUESTED': return {
+      ...state,
+      isFetching: true,
+      isError: false,
+      controller: action.payload
+    }
+    case 'FETCH_SUCCEEDED': return {
+      ...state,
+      response: action.payload.response,
+      data: action.payload.data,
+      isFetching: false,
+      isError: false,
+      controller: null
+    }
+    case 'FETCH_FAILED': return {
+      ...state,
+      response: action.payload.response,
+      isFetching: false,
+      isError: true,
+      data: null,
+      controller: null
+    }
+    default: throw Error;
+  }
+}
+
+interface FetchRequested {
+  type: 'FETCH_REQUESTED'
+  payload: AbortController
+}
+
+interface FetchSucceeded<T> {
+  type: 'FETCH_SUCCEEDED'
+  payload: {
+    data: T
+    response: Response
+  }
+}
+
+interface FetchFailed {
+  type: 'FETCH_FAILED'
+  payload: {
+    response: Response
+  }
+}
+
+export type FetchAction<T> =
+  | FetchRequested
+  | FetchSucceeded<T>
+  | FetchFailed;
